cc10b7533d9a5269a69b4c4ecf6a91fcc3b2e84f
Arushi <aruash7551@gmail.com>
2025-03-31 12:07:30 +0530

    changing the andriod platform versions

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,8 +7,8 @@ plugins {
 
 android {
     namespace = "com.example.waste_management"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 35
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -24,8 +24,9 @@ android {
         applicationId = "com.example.waste_management"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+
+        minSdk = 33
+        targetSdk = 33
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
